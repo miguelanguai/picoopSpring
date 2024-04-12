ab222445a585757ca39ac1edf3015fb36be89f5b
Miguel Angel <miguelangelguaivi@gmail.com>
2024-04-12 19:15:20 +0200

    feat: first commit, creation of server side spring

---
+++ src/main/java/project/picoop/PicoopApplication.java
@@ -0,0 +1,13 @@
+package project.picoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicoopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/picoop/PicoopApplicationTests.java
@@ -0,0 +1,13 @@
+package project.picoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicoopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
